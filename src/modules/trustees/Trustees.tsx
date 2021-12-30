460bc4d43bc0d7aa021e7d9c8b2d02f2387e266c
PiusKariuki <piuskariuki19@gmail.com>
2021-12-30 18:42:03 +0300

    trustees

--- src/modules/trustees/Trustees.tsx
+++ src/modules/trustees/Trustees.tsx
@@ -42,26 +42,84 @@ const Trustees: React.FC = (): JSX.Element => {
       {/* .................working............ */}
       <div
         className="flex flex-col px-[24px] pt-[48px] pb-[24px] gap-[3rem] justify-center
-      lg:py-[96px] lg:px-[330px]"
+      lg:py-[96px] lg:px-[180px]"
       >
         <p
           className="text-blue-80 font-[600] text-[32px] leading-[40px] lg:text-[56px] 
         lg:leading-[64px] text-center "
         >
-          Working with data doesn't need to be hard
+          Working with data doesn't <br /> need to be hard
         </p>
         <p
-          className="text-center font-[600] text-blue-80 text-[18px] leading-[32px] lg:text-[20px] 
-        lg:leading-[32px] lg:px-[100px]"
+          className="text-center font-[600] text-blue-80 text-[18px] leading-[32px] 
+          lg:text-[20px]  lg:leading-[32px]"
         >
-          Our guiding principle is to help developers solve their data
-          challenges. Here’s what you can do with MongoDB.
+          Our guiding principle is to help developers solve <br /> their data
+          challenges. Here’s what you can do <br /> with MongoDB.
         </p>
       </div>
 
       {/* ........................................................... */}
-      <div className="">
-          
+      <div
+        className="flex flex-col flex-wrap w-full md:flex-nowrap md:flex-row space-around
+        px-[24px]  pb-[24px] md:px-[32px] lg:px-[48px] gap-[16px] 460:gap-[32px]
+         md:gap-[48px]"
+      >
+          {/* ......................................................... */}
+        <div className="flex flex-col  md:py-[80px] self-center">
+          <img
+            src="/img/faster.svg"
+            alt="faster"
+            className="w-[64px] h-[64px] self-center"
+          />
+          <p
+            className="text-blue-80  text-center text-[18px] leading-[28px] font-[900]
+            lg:text-[24px] lg:leading-[32px] py-[12px]"
+          >
+            Build Faster
+          </p>
+          <p className="text-center text-[14px] font-[600] lg:text-[16px] lg:leading-[32px] py-[12px]">
+            Ship and iterate 3–5x faster with our flexible document data model
+            and a unified query interface for any use case.
+          </p>
+        </div>
+        {/* .............................................................. */}
+        <div className="flex flex-col  py-[80px] self-center">
+          <img
+            src="/img/scale.svg"
+            alt="faster"
+            className="w-[64px] h-[64px] self-center"
+          />
+          <p
+            className="text-blue-80  text-center text-[18px] leading-[28px] font-[900]
+            lg:text-[24px] lg:leading-[32px] py-[12px]"
+          >
+            Scale Further
+          </p>
+          <p className="text-center text-[14px] font-[600] lg:text-[16px] lg:leading-[32px] py-[12px]">
+            Whether it’s your first customer or 20 million users around the
+            world, meet your performance SLAs in any environment.
+          </p>
+        </div>
+        {/* .............................................................. */}
+        <div className="flex flex-col  py-[80px] self-center">
+          <img
+            src="/img/security.svg"
+            alt="faster"
+            className="w-[64px] h-[64px] self-center"
+          />
+          <p
+            className="text-blue-80  text-center text-[18px] leading-[28px] font-[900]
+            lg:text-[24px] lg:leading-[32px] py-[12px]"
+          >
+            Sleep Better
+          </p>
+          <p className="text-center text-[14px] font-[600] lg:text-[16px] lg:leading-[32px] py-[12px]">
+            Easily ensure high availability, protect data integrity, and meet
+            the security and compliance standards for your mission-critical
+            workloads.
+          </p>
+        </div>
       </div>
     </div>
   );
